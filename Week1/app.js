a6c8af8d4e9400ce9be17265c00c459a9b8f6f50
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2016-09-27 00:02:16 +0200

    My private tests :)

---
+++ Week1/app.js
@@ -0,0 +1,22 @@
+(function () {
+'use strict';
+
+angular.module("NameCalculator", [])
+.controller("NameCalculatorController", function($scope){
+  $scope.name="";
+  $scope.totalValue=0;
+  $scope.displayNumeric = function(){
+    var total = calculateNumeric($scope.name);
+    $scope.totalValue = total;
+
+  };
+  function calculateNumeric(string){
+    var totalStringValue=0;
+    for (var i = 0; i < string.length; i++) {
+      totalStringValue += string.charCodeAt(i);
+    }
+    return totalStringValue;
+  }
+});
+
+})();
